13ee1c89051c36fec3267dcf569092a198139fee
Lapinas <mrlapinas99@gmail.com>
2022-01-05 17:12:44 +0000

    ....

--- index.js
+++ index.js
@@ -19,12 +19,24 @@ document.addEventListener("keypress", (event2) => {
 
 document.addEventListener("keypress", (event2) => {
     if (event2.key == event2.key) {
-        textInnit2.textContent = event2.keyCode;
+        textInnit2.textContent = event2.code;
     }
 });
 
 document.addEventListener("keypress", (event) => {
     if (event.key == event.key) {
-        textInnit3.textContent = event.code;
+        textInnit3.textContent = event.keyCode;
+    }
+});
+
+document.addEventListener("keypress", (event) => {
+    if (event.key == 3) {
+        const thisHaha = document.getElementById("audio");
+        thisHaha.volume = 0.1;
+        thisHaha.play();
+    } else if (event.key == "w") {
+        const thisHaha2 = document.getElementById("audio2");
+        thisHaha2.volume = 0.1;
+        thisHaha2.play();
     }
 });
