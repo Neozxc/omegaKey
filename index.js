4b579680d9323087a6e97a4c8669398dea0a56d7
Lapinas <mrlapinas99@gmail.com>
2022-01-05 17:21:27 +0000

    final

--- index.js
+++ index.js
@@ -5,36 +5,32 @@ const textInnit2 = document.getElementById("appearsLater2");
 const textInnit3 = document.getElementById("appearsLater3");
 
 document.addEventListener("keypress", (event) => {
-    if (event.key == event.key) {
+    if (event.key) {
         random.textContent = event.keyCode;
         console.log(`${event.key} was pressed.`, event);
     }
 });
 
 document.addEventListener("keypress", (event2) => {
-    if (event2.key == event2.key) {
+    if (event2.key) {
         textInnit.textContent = event2.key;
     }
 });
 
-document.addEventListener("keypress", (event2) => {
-    if (event2.key == event2.key) {
-        textInnit2.textContent = event2.code;
+document.addEventListener("keypress", (event3) => {
+    if (event3.key) {
+        textInnit2.textContent = event3.code;
     }
 });
 
-document.addEventListener("keypress", (event) => {
-    if (event.key == event.key) {
-        textInnit3.textContent = event.keyCode;
+document.addEventListener("keypress", (event4) => {
+    if (event4.key) {
+        textInnit3.textContent = event4.keyCode;
     }
 });
 
-document.addEventListener("keypress", (event) => {
-    if (event.key == 3) {
-        const thisHaha = document.getElementById("audio");
-        thisHaha.volume = 0.1;
-        thisHaha.play();
-    } else if (event.key == "w") {
+document.addEventListener("keypress", (event5) => {
+if (event5.key == "w".toLocaleLowerCase() || event5.key == "w".toLocaleUpperCase()) {
         const thisHaha2 = document.getElementById("audio2");
         thisHaha2.volume = 0.1;
         thisHaha2.play();
